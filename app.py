4cb305322e39221260fab40d846625b622f759f5
Bill Hicks <billvanhausen@users.noreply.github.com>
2025-12-24 07:41:25 -0500

    refactor app functions

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 # project_root/app.py
 import tkinter as tk
+from tkinter import messagebox
+from tkinter import filedialog
 from modules.views import My_frame
 from modules.menus import Menubar
 
 class App(tk.Tk):
+    # App Initializations
     def __init__(self):
         super().__init__()
 
-        # Title, icon, size
         self.title('Tkinter - Object Oriented Programming')
         self.iconbitmap('./images/digitalhicks.ico')
         self.geometry('600x400')
@@ -18,10 +20,19 @@ class App(tk.Tk):
         # Create a frame outside of the __init__ function
         My_frame(self).pack(pady=20)
 
+    # App functions
     def file_exit(self):
         self.destroy()
 
+    def about_message(self):
+        messagebox.showinfo('About Parent', '©2025 Bill Hicks')
 
+    def file_open(self):
+        file = filedialog.askopenfile('r')
+        contents = open(file.name)
+        messagebox.showinfo('File Contents', contents.read())
+
+# Start App Main Loop
 if __name__ == '__main__':
     app = App()
     app.mainloop()
--- modules/menus.py
+++ modules/menus.py
@@ -1,17 +1,5 @@
 # project_root/modules/menus.py
 import tkinter as tk
-from tkinter import messagebox
-
-def file_open():
-    pass
-
-# def file_exit(parent):
-#     print('File Exit')
-#     print(str(parent))
-#     # parent.destroy()
-
-def about_message():
-    messagebox.showinfo('About', '©2025 Bill Hicks')
 
 class Menubar(tk.Menu):
     def __init__(self, parent):
@@ -19,7 +7,7 @@ class Menubar(tk.Menu):
 
         file_items = tk.Menu(tearoff=False)
         file_items.add_command(label='New')
-        file_items.add_command(label='Open', command=file_open)
+        file_items.add_command(label='Open', command=parent.file_open)
         file_items.add_command(label='Save')
         file_items.add_separator()
         file_items.add_command(label='Exit', command=parent.file_exit)
@@ -28,7 +16,7 @@ class Menubar(tk.Menu):
         run_items = tk.Menu(tearoff=False)
 
         help_items = tk.Menu(tearoff=False)
-        help_items.add_command(label='About', command=about_message)
+        help_items.add_command(label='About', command=parent.about_message)
 
         self.add_cascade(label='File', menu=file_items)
         self.add_cascade(label='Edit', menu=edit_items)
